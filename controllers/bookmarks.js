00d419891c4cf8307f2d8d88ae06a35c642ffa06
AntMousinho <antony.mousinho@hotmail.co.uk>
2021-04-29 18:01:19 +0100

    made it look amazing!

--- controllers/bookmarks.js
+++ controllers/bookmarks.js
@@ -2,33 +2,23 @@ const express = require("express");
 let router = express.Router();
 
 const Bookmark = require('../models').bookmark
-router.use(express.static(__dirname + '/public'));
-
 
 router.get("/", async function (req, res) {
-  const bookmarks = await Bookmark.findAll();
-
-//   res.render("pages/bookmarks", {
-//     //Bookmarks: bookmarks.map(bookList => bookList.dataValues.url) 
-//     //Because the table contents are stored in an object inside an array, we need to map.
-//     Bookmarks: bookmarks.map(bookList => bookList.url)
-//   });
-   
+	const bookmarks = await Bookmark.findAll();
 	res.render('pages/bookmarks.ejs', { bookmarks: bookmarks });
-
 });
 
-router.post("/", async function (req,res) {
+router.post("/", async function (req, res) {
 
-  await Bookmark.create(
-    {
-      url: req.body.url,
-    }
-  )
+	await Bookmark.create(
+		{
+			url: req.body.url,
+		}
+	)
 
-  const bookmarks = await Bookmark.findAll();
+	const bookmarks = await Bookmark.findAll();
 
-  res.render("pages/bookmarks", { bookmarks: bookmarks })
+	res.render("pages/bookmarks", { bookmarks: bookmarks })
 })
 
 router.delete('/:bookmarkId', async (req, res) => {
@@ -47,7 +37,7 @@ router.get('/edit/:bookmarkId', async (req, res) => {
 			id: req.params.bookmarkId
 		}
 	})
-	res.render("pages/oneBookmark", { bookmark: bookmarks});
+	res.render("pages/oneBookmark", { bookmark: bookmarks });
 })
 
 router.put('/:bookmarkId', async (req, res) => {
